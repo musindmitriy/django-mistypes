fb79f2b20db1e7a39fa8abf6978af40b733b3fff
dmitriym <dmitry.musin@phystech.edu>
2016-07-26 22:13:32 +0300

    Improve translations

--- mistypes/signals.py
+++ mistypes/signals.py
@@ -1,4 +1,6 @@
 
+import threading
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import mail_admins
@@ -8,6 +10,24 @@ from django.utils.translation import ugettext_lazy as _
 from .models import Mistype
 
 
+class MailThread(threading.Thread):
+    def __init__(self, mistype, **kwargs):
+        self.mistype = mistype
+        super(MailThread, self).__init__(**kwargs)
+
+    def run(self):
+        mistype = self.mistype
+        context = {
+            'url': mistype.url,
+            'ip': mistype.ip,
+            'mistype': mistype.full_text(),
+            'comment': mistype.comment
+        }
+
+        mail_admins(_('New mistype'), '',
+                    html_message=render_to_string('mistypes/mistype-email.html', context))
+
+
 @receiver(post_save, sender=Mistype)
 def email_notifier(sender, **kwargs):
 
@@ -15,12 +35,5 @@ def email_notifier(sender, **kwargs):
         return  # Send email only on new mistypes
 
     mistype = kwargs["instance"]
-    context = {
-        'url': mistype.url,
-        'ip': mistype.ip,
-        'mistype': mistype.full_text(),
-        'comment': mistype.comment
-    }
+    MailThread(mistype).start()
 
-    mail_admins(_('New mistype'), '',
-                html_message=render_to_string('mistypes/mistype-email.html', context))
